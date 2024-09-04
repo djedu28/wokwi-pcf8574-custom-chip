8e1fa74daffb0d682bdf2f13af871df6dd3de39a
Eduardo Santos <43933482+djedu28@users.noreply.github.com>
2024-09-04 03:39:49 -0300

    Update: address = dynamic, based on A0, A1 and A2
    Now the address is dynamic, based on A0, A1 and A2
    Agora o endereço é dinâmico, baseado em A0, A1 e A2

--- src/main.c
+++ src/main.c
@@ -8,24 +8,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const int ADDRESS = 0x38;
+const int ADDRESS = 0x20; // Default (A0, A1 and A2 = 0)
 
 typedef struct {
-    pin_t PIN_SDA;
-    pin_t PIN_SCL;
-    pin_t PIN_INT;
-    pin_t PIN_A0;
-    pin_t PIN_A1;
-    pin_t PIN_A2;
-    pin_t PIN_P0;
-    pin_t PIN_P1;
-    pin_t PIN_P2;
-    pin_t PIN_P3;
-    pin_t PIN_P4;
-    pin_t PIN_P5;
-    pin_t PIN_P6;
-    pin_t PIN_P7;
-    uint8_t counter;
+  pin_t PIN_SDA;
+  pin_t PIN_SCL;
+  pin_t PIN_INT;
+  pin_t PIN_A0;
+  pin_t PIN_A1;
+  pin_t PIN_A2;
+  pin_t PIN_P0;
+  pin_t PIN_P1;
+  pin_t PIN_P2;
+  pin_t PIN_P3;
+  pin_t PIN_P4;
+  pin_t PIN_P5;
+  pin_t PIN_P6;
+  pin_t PIN_P7;
+  uint8_t counter;
+  uint8_t address;
+  bool stk;
 } chip_state_t;
 
 static bool on_i2c_connect(void *user_data, uint32_t address, bool connect);
@@ -40,19 +42,46 @@ void chip_init() {
   chip->PIN_A0 = pin_init("A0", INPUT);
   chip->PIN_A1 = pin_init("A1", INPUT);
   chip->PIN_A2 = pin_init("A2", INPUT);
-  chip->PIN_P0 = pin_init("P0", OUTPUT);
-  chip->PIN_P1 = pin_init("P1", OUTPUT);
-  chip->PIN_P2 = pin_init("P2", OUTPUT);
+  chip->PIN_P0 = pin_init("P0", INPUT_PULLUP);
+  chip->PIN_P1 = pin_init("P1", INPUT_PULLUP);
+  chip->PIN_P2 = pin_init("P2", INPUT_PULLUP);
   chip->PIN_P3 = pin_init("P3", OUTPUT);
   chip->PIN_P4 = pin_init("P4", OUTPUT);
   chip->PIN_P5 = pin_init("P5", OUTPUT);
   chip->PIN_P6 = pin_init("P6", OUTPUT);
   chip->PIN_P7 = pin_init("P7", OUTPUT);
   chip->counter = 0;
+  chip->address = ADDRESS;
+
+
+  uint8_t mask = 0x000;
+
+  if (pin_read(chip->PIN_A0) == HIGH) {
+    mask |= 1 << 2;
+  }
+
+  if (pin_read(chip->PIN_A1) == HIGH) {
+    mask |= 1 << 1;
+  }
+
+  if (pin_read(chip->PIN_A2) == HIGH) {
+    mask |= 1 << 0;
+  }
+
+  switch (mask) {
+    case 0: chip->address = 0x20; break;
+    case 1: chip->address = 0x24; break;
+    case 2: chip->address = 0x22; break;
+    case 3: chip->address = 0x26; break;
+    case 4: chip->address = 0x21; break;
+    case 5: chip->address = 0x25; break;
+    case 6: chip->address = 0x23; break;
+    case 7: chip->address = 0x27; break;
+  }
 
   const i2c_config_t i2c_config = {
     .user_data = chip,
-    .address = ADDRESS,
+    .address = chip->address,
     .scl = pin_init("SCL", INPUT),
     .sda = pin_init("SDA", INPUT),
     .connect = on_i2c_connect,
@@ -65,7 +94,7 @@ void chip_init() {
   
   // The following message will appear in the browser's DevTools console:
   // printf("Hello from custom chip!\n");
-  printf("Hello from PCF8574 at address %x \n", ADDRESS);
+  printf("Hello from PCF8574 at address %x \n", chip->address);
 }
 
 
@@ -134,14 +163,16 @@ uint8_t on_i2c_read(void *user_data) {
   chip->counter<<=1;
   chip->counter |= pin_read(chip->PIN_P0);
   
+  //printf("on_i2c_read %hhx \n", chip->counter);
   return chip->counter;
 }
 
 bool on_i2c_write(void *user_data, uint8_t data) {
   chip_state_t *chip = user_data;
+  //printf("on_i2c_write %hhx \n", data);
   chip->counter = data;
   update_pins(chip);
-  return true; // Ack
+  return true;// Ack
 }
 
 
